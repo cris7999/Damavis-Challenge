1d131f654a30fa4712b294c90bd0d83baef010d2
cris7999 <cris7999@hotmail.com>
2022-05-13 23:55:57 +0200

    Create snake_challengue.py
    First solution

---
+++ snake_challengue.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+def printSnake(board, snake):
+    print("-----")
+    for x in range(0, board[0]):
+        for y in range(0, board[1]):
+            if ([x, y] in snake):
+                if ([x, y] == snake[0]):
+                    print("C", end="")
+                else:
+                    print("X", end="")
+
+            else:
+                print("O", end="")
+        print("")
+    print("-----")
+
+
+def inBoard(board, corX, corY):
+    if corX >= 0 and corY >= 0 and corX < board[0] and corY < board[1]:
+        return True
+    else:
+        return False
+
+
+validPathLength = 0
+def numberOfAvailableDifferentPaths(board, snake, depth):
+    global validPathLength
+    around_array = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+    if depth >0:
+        for around in around_array:
+            future_mov = list(np.add(snake[0], around))
+            tail = snake.pop()
+
+            if (future_mov not in snake) and inBoard(board,future_mov[0],future_mov[1]):
+                snakeModified = [future_mov] + snake
+                snake.append(tail)
+                printSnake(board,snakeModified)
+                numberOfAvailableDifferentPaths(board, snakeModified, depth - 1)
+
+                if depth == 1:
+                    validPathLength = validPathLength + 1
+
+            else:
+                snake.append(tail)
+
+
+board = [4,3]
+snake =[[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]]
+depth = 3
+
+print("Initial pos")
+printSnake(board,snake)
+numberOfAvailableDifferentPaths(board, snake, depth)
+
+print("Final result:" + str(validPathLength))
